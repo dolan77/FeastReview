31d4a33e5e88f5bcbadd528fa659d23c7f7aefbf
matthewhirai <mhirai301@gmail.com>
2022-12-11 14:17:03 -0800

    created basic login and sign up page

--- App.js
+++ App.js
@@ -1,80 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/core';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-	createUser = () => {
-		auth()
-			.createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-			.then(() => {
-				console.log('User account created & signed in!');
-			})
-			.catch(error => {
-				if (error.code === 'auth/email-already-in-use') {
-				console.log('That email address is already in use!');
-				}
-
-				if (error.code === 'auth/invalid-email') {
-				console.log('That email address is invalid!');
-				}
-
-				console.error(error);
-			});
-	}
-
-	login = () => {
-		auth()
-			.signInWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-			.then(() => {
-				console.log('User signed in!');
-			})
-			.catch(error => {
-				if (error.code === 'auth/invalid-email') {
-				console.log('That email address is invalid!');
-				}
-
-				console.error(error);
-			});
-	}
-
-	logoff = () => {
-		auth()
-			.signOut()
-			.then(() => console.log('User signed out!'));
-	}
-
-	// Set an initializing state whilst Firebase connects
-	const [initializing, setInitializing] = useState(true);
-	const [user, setUser] = useState();
-
-	// Handle user state changes
-	function onAuthStateChanged(user) {
-		setUser(user);
-		if (initializing) setInitializing(false);
-	}
-
-	useEffect(() => {
-		const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-		return subscriber; // unsubscribe on unmount
-	}, []);
-
-	if (initializing) return null;
-
-	if (!user) {
-		return (
-		<View>
-			<Text>Login</Text>
-			<Button title="Create User" onPress={this.createUser} />
-			<Button title="Login" onPress={this.login} />
-		</View>
-		);
-	}
-
 	return (
-		<View>
-			<Text>Welcome {user.email}</Text>
-			<Button title="Create User" onPress={this.createUser} />
-			<Button title="Log off" onPress={this.logoff} />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+				<Stack.Screen name="Home" component={HomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/core';
+
+export default function HomeScreen() {
+	const navigation = useNavigation()
+
+	logoff = () => {
+		auth()
+			.signOut()
+			.then(() => {
+				navigation.replace("Login")
+			})
+			.catch(error => alert(error.message))
+	}
+
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity 
+				style={styles.button}
+				onPress={logoff}
+			>
+				<Text style={styles.buttonText}>Sign Out</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	button: {
+		backgroundColor: '#0782F9',
+		width: '100%',
+		padding: 15,
+		borderRadius: 10,
+		alignItems: 'center'
+	},
+	buttonText: {
+		color: 'white',
+		fontWeight: '700',
+		fontSize: 16
+	}
+})
