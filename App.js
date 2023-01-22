77cbb730752567e417720c10bd14d1d18fc56926
matthewhirai <mhirai301@gmail.com>
2023-01-21 16:34:58 -0800

    created register screen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import { RegisterScreen } from './screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -14,6 +15,7 @@ export default function App() {
 		<NavigationContainer>
 			<Stack.Navigator>
 				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+				<Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
 				<Stack.Screen name="Home" component={HomeScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
