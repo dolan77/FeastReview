e57dc2e39ee00d5f82409cb673de98f19714509a
matthewhirai <mhirai301@gmail.com>
2023-01-26 18:10:28 -0800

    added header options to home screen

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
 			<Stack.Navigator>
 				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
 				<Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
