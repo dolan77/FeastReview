9d8091cee77521634d84187af08c80e2b05431f1
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-01-26 16:17:25 -0800

    Navbar and icons work, needs integration... NavBar needs to be integrated with the Stack Navigator. Currently having error when loading UserProfileScreen, may need to do previously mentioned integration to fix it.

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import auth from '@react-native-firebase/auth';
-import { useNavigation } from '@react-navigation/core';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
+// import React, { useState, useEffect } from 'react';
+// import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import auth from '@react-native-firebase/auth';
+// import { useNavigation } from '@react-navigation/core';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import LoginScreen from './screens/LoginScreen';
+// import HomeScreen from './screens/HomeScreen';
 
-const Stack = createNativeStackNavigator()
+// const Stack = createNativeStackNavigator()
 
-export default function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-				<Stack.Screen name="Home" component={HomeScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
+// export default function App() {
+// 	return (
+// 		<NavigationContainer>
+// 			<Stack.Navigator>
+// 				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+// 				<Stack.Screen name="Home" component={HomeScreen} />
+// 			</Stack.Navigator>
+// 		</NavigationContainer>
+// 	);
+// }
+
+import * as React from 'react';
+import NavBar from './screens/NavBar';
+
+function App() {
+	return(
+		<NavBar/>
 	);
 }
+
+export default App;
