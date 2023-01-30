e0fb5daf915ef896957c87854112a8bbd5fb15e4
blazeris <blazeris@users.noreply.github.com>
2023-01-29 18:51:25 -0800

    documentation on firebase and yelp

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,25 +1,31 @@
-//const { initializeApp, cert } = require('firebase-admin/app');
-//const { getFirestore } = require('firebase-admin/firestore');
 import firestore from '@react-native-firebase/firestore';
-//const apiKey = require('../../api_keys.json').firebase;
 
 module.exports = {dbGet, dbSet};
 
 /**
- * a utility class to simply usage of firebase functions
+ * a utility class to simplify usage of firebase functions
  */
-
-// example of usage
-//get('api_keys', 'key').then((result) => console.log(result));
 
 
 const db = firestore();
 
+/**
+ * simple getter function
+ * @param {*} collection 
+ * @param {*} doc 
+ * @returns the object stored in the collection's document
+ */
 async function dbGet(collection, doc){
     const docSnapshot = await db.collection(collection).doc(doc).get();
     return docSnapshot.data();
 }
 
+/**
+ * simple setter
+ * @param {*} collection 
+ * @param {*} doc 
+ * @param {*} value the object overwriting the collection's doc value
+ */
 async function dbSet(collection, doc, value){
     const docRef = db.collection(collection).doc(doc);
     await docRef.set(value);
