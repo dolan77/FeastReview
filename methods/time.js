c2aba09f5975ac2f2a47de40e4ed1d8a75ef35d2
matthewhirai <mhirai301@gmail.com>
2023-02-06 21:54:06 -0800

    added star rating and time convert files

---
+++ methods/time.js
@@ -0,0 +1,14 @@
+export default timeConvert = (time) => {
+	// Check correct time format and split into components
+	time = time.toString ().match (/^([01]\d|2[0-3])([0-5]\d)?$/) || [time]
+
+	if (time.length > 1) {
+	  time = time.slice (1);  // Remove full string match value
+	  time[3] = + time[0] < 12 ? 'AM' : 'PM' // Set AM/PM
+	  time[1] = ':' + time[1] // add :
+	  time[0] = + time[0] % 12 || 12 // Adjust hours
+	}
+	
+	return time.join('')
+}
+
