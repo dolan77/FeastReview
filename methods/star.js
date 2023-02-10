530aa4ae5779229534e97cb72584e90169466d70
matthewhirai <mhirai301@gmail.com>
2023-02-09 17:24:45 -0800

    updated methods

--- methods/star.js
+++ methods/star.js
@@ -3,11 +3,11 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 export const starRating = (id, rating) => {
 	let stars = []
 	for(let i = 0; i < Math.floor(rating); i++) {
-		stars.push(<Ionicons key={id + i} name="star" color="white" size={20} />);
+		stars.push(<Ionicons key={id + i} name="star" color="gold" size={20} />);
 	}
 
 	if (rating % 1 !== 0) {
-		stars.push(<Ionicons key={id + '0.5'} name="star-half" color="white" size={20} />);
+		stars.push(<Ionicons key={id + '0.5'} name="star-half" color="gold" size={20} />);
 	}
 
 	return stars
