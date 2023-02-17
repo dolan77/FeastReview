dfce3d411c675fb6eff6678c461f17b66ae3cc71
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-02-16 22:25:55 -0800

    Revert "NavBar updates in progress"
    This reverts commit af6d282c45cd3e301cd70d6616e8fcfd1049edab.

--- screens/NavBar.js
+++ screens/NavBar.js
@@ -1,27 +1,11 @@
 import * as React from 'react';
-
-
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-// import { Ionicons } from '@expo/vector-icons';
 
 // Screens
+import HomeScreen from './HomeScreen';
 import ProfileScreen from './UserProfileScreen';
 import SearchScreen from './SearchScreen';
-import LoginScreen from './LoginScreen';
-import HomeScreen from './HomeScreen';
-import UserProfileScreen from './UserProfileScreen';
-import ReviewsScreen from './ReviewsScreen';
-import MessagesScreen from './MessagesScreen';
-import FollowersScreen from './FollowersScreen';
-import FollowingScreen from './FollowingScreen';
-import RestaurantProfileScreen from './RestaurantProfileScreen';
-import OtherUserProfileScreen from './OtherUserProfileScreen';
-import RegisterScreen from './RegisterScreen';
-import CreateReviewScreen from './CreateReviewScreen';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 // Screen names
@@ -29,9 +13,10 @@ const homeName = 'Home';
 const searchName = 'Search';
 const profileName = 'Profile';
 
-const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator();
 
+// Creates a bottom tab navigator with three buttons: home, search, and outline. Pressing each of the buttons will
+// take you to its corresponding screens.
 export default function NavBar() {
     return(
             <Tab.Navigator
@@ -53,32 +38,13 @@ export default function NavBar() {
 
                         return <Ionicons name = {iconName} size = {size} color = {color}/>  // This can be put on the individual icons above for custom effects
                     }
-                })}>
+            })}>
 
-                <Tab.Screen name={homeName} component={StackScreens}/>
-                <Tab.Screen name={searchName} component={StackScreens}/>   
-                <Tab.Screen name={profileName} component={StackScreens}/>
+            <Tab.Screen name={homeName} component={HomeScreen}/>
+            <Tab.Screen name={searchName} component={SearchScreen}/>   
+            <Tab.Screen name={profileName} component={ProfileScreen}/>
 
 
             </Tab.Navigator>
     )
-
-	function StackScreens(){
-		return (
-			<Stack.Navigator initialRouteName='Login'>
-				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-		        <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Your Profile" component={UserProfileScreen} />
-				<Stack.Screen name="Reviews" component={ReviewsScreen} />
-				<Stack.Screen name="Messages" component={MessagesScreen} />
-				<Stack.Screen name="Followers" component={FollowersScreen} />
-				<Stack.Screen name="Following" component={FollowingScreen} />
-				<Stack.Screen name="RestaurantProfile" component={RestaurantProfileScreen} />
-				<Stack.Screen name="OtherUserProfile" component={OtherUserProfileScreen}/>
-                <Stack.Screen name="Search" component={SearchScreen}/>
-                <Stack.Screen name="Create Review" component={CreateReviewScreen} />
-			</Stack.Navigator>
-		)
-	}
 }
