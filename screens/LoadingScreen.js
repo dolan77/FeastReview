6d1eb58a59232d9a32f3ad091e853866ccd9a38a
matthewhirai <mhirai301@gmail.com>
2023-02-19 20:36:38 -0800

    loading indicator completed

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -2,40 +2,45 @@ import React, { useState, useEffect } from 'react';
 import { Text, View} from 'react-native'
 import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/core';
-import { dbSet } from '../utils/firebase'
+import * as firebase from '../utils/firebase'
+import Loader from '../methods/Loader'
 
 export default function LoadingScreen() {
 	const navigation = useNavigation()
 
 	useEffect(() => {
-		const subscriber = auth().onAuthStateChanged((user) => {
-			console.log("user", JSON.stringify(user));
-			dbSet(
-				"users", 
-				user.uid, 
-				{
-					bio: "",
-					bookmarks: [],
-					expertise: {},
-					followers: [],
-					following: [],
-					username: user.displayName,
-					reviews: []
-				}
-			).then(() => {
-				navigation.navigate("TabNavigator")
-			})
+		const unsubscribe = auth().onAuthStateChanged((user) => {
+			redirect(user)
 		});
 
-		return subscriber;
+		return unsubscribe;
 	}, []);
 
+	const redirect = (user) => {
+		addUserToDb(user).then(() => {
+			setTimeout(() => {
+				navigation.navigate("TabNavigator")
+			}, 1000);
+		})
+	}
 
+	const addUserToDb = async (user) => {
+		const res = await firebase.dbSet(
+			"users", 
+			user.uid, 
+			{
+				bio: "",
+				bookmarks: [],
+				expertise: {},
+				followers: [],
+				following: [],
+				username: user.displayName,
+				reviews: []
+			}
+		)
+	}
+	
     return(
-        <View>
-            <View>
-                <Text>LOADING...</Text>
-            </View>
-        </View>
+        <Loader />
     );
 }
