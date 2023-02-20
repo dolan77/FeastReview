ed72e84dfeaa8a4c036ffe563a3956c693526ec3
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-02-20 12:33:12 -0800

    NavBar shows on current subpages. Still need to complete for search

--- App.js
+++ App.js
@@ -31,15 +31,6 @@ export default function App() {
 				<Stack.Screen options={{ headerShown: false }} name="TabNavigator" component={NavTabs} />
 				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
 		        <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Your Profile" component={UserProfileScreen} />
-				<Stack.Screen name="Reviews" component={ReviewsScreen} />
-				<Stack.Screen name="Messages" component={MessagesScreen} />
-				<Stack.Screen name="Followers" component={FollowersScreen} />
-				<Stack.Screen name="Following" component={FollowingScreen} />
-				<Stack.Screen name="RestaurantProfile" component={RestaurantProfileScreen} />
-				<Stack.Screen name="OtherUserProfile" component={OtherUserProfileScreen}/>
-				<Stack.Screen name="Create Review" component={CreateReviewScreen} />
 				
 			</Stack.Navigator>
 		</NavigationContainer>
