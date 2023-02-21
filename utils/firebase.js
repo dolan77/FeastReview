98fce70621e9a7f0d1c1b1a2e7c89afd6a6e37c2
blazeris <blazeris@users.noreply.github.com>
2023-02-21 14:43:50 -0800

    firebase query for followers

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,7 +2,7 @@ import firestore, { firebase } from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
 //import firebase from '@react-native-firebase/app';
 
-module.exports = {dbGet, dbSet, dbFileGetUrl, dbFileAdd, dbGetReviews, dbUpdate, dbUpdateOnce, dbUpdateArrayAdd, dbUpdateArrayRemove};
+module.exports = {dbGet, dbSet, dbFileGetUrl, dbFileAdd, dbGetReviews, dbUpdate, dbUpdateOnce, dbUpdateArrayAdd, dbUpdateArrayRemove, dbGetFollowers};
 
 
 /**
@@ -161,4 +161,23 @@ async function dbUpdateArrayAdd(collection, doc, field, values){
  */
 async function dbUpdateArrayRemove(collection, doc, field, values){
     return dbUpdateOnce(collection, doc, field, firestore.FieldValue.arrayRemove(...values));
+}
+
+
+/**
+ * acquires a map of all users that follow a specific user by id
+ * @param {*} uid user id we are getting followers of
+ * @returns 
+ */
+async function dbGetFollowers(uid){
+    console.log(uid)
+    let query = await db.collection("users").where("following", "array-contains", uid).get().then((user_query) => {
+        let followers = new Map();
+        user_query.docs.forEach((user_query) => {
+            followers.set(user_query.ref.id, user_query.data());
+        });
+        return followers;   
+    });
+    
+    return query? query: new Map();
 }
