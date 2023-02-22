90d919fc1387c60a808baffe89d9d589ca26fa7f
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-02-21 17:11:04 -0800

    Cleanup App.js imports

--- App.js
+++ App.js
@@ -5,19 +5,8 @@ import { useNavigation } from '@react-navigation/core';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import UserProfileScreen from './screens/UserProfileScreen';
-import ReviewsScreen from './screens/ReviewsScreen';
-import MessagesScreen from './screens/MessagesScreen';
-import FollowersScreen from './screens/FollowersScreen';
-import FollowingScreen from './screens/FollowingScreen';
-import RestaurantProfileScreen from './screens/RestaurantProfileScreen';
 import NavBar from './screens/NavBar';
-import OtherUserProfileScreen from './screens/OtherUserProfileScreen';
-import CreateReviewScreen from './screens/CreateReviewScreen';
-
 import RegisterScreen from './screens/RegisterScreen';
-import FollowersAndFollowing from './screens/FollowersAndFollowingScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -32,7 +21,6 @@ export default function App() {
 				<Stack.Screen options={{ headerShown: false }} name="TabNavigator" component={NavTabs} />
 				<Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
 		        <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-				
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
