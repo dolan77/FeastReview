4318c175f716a924cf69b4e8c11fd3db2053dccd
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-04-01 18:44:37 -0700

    Fixes

--- screens/CreateReviewScreen.js
+++ screens/CreateReviewScreen.js
@@ -33,6 +33,7 @@ export default function ReviewPage({route}) {
                                                         datemade: new Date(),
                                                         image_urls: Get_Image_URLs(review_id),
                                                         restaurant_alias: restaurantData.data.alias,
+                                                        restaurant_name: restaurantData.data.name,
                                                         star_atmos: atmosphereDefaultRating,
                                                         star_foods: foodDefaultRating,
                                                         star_service: serviceDefaultRating
@@ -50,7 +51,7 @@ export default function ReviewPage({route}) {
     // Uploads user's review photos to the database storage.
     async function uploadPhotos(){
         try{
-            images.map((image, index) => firebase.dbFileAdd(image_urls[index], image.path))
+            images.map((image, index) => firebase.dbFileAdd('ReviewPhotos/' + image_urls[index], image.path))
         }
         catch (error){
             console.log(error)
