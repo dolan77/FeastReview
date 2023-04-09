f8da06073cd5fd9c68166972a3b7fe3ed177397e
Dolan <dylanhuynh77@gmail.com>
2023-04-08 21:45:29 -0700

    [add] Updated UI of Search Screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -217,7 +217,7 @@ export default function SearchScreen() {
 					/>
 				}
 			</View>
-
+			{/* Modal View */}
 			{modalVisible && 
 				<Modal
 					animationType="slide"
@@ -229,8 +229,14 @@ export default function SearchScreen() {
 				>
 					{/* Container for all filter components */}
 					<View style = {styles.modalView}>
-						<Text style={[styles.buttonText, {fontSize: 30}]} onPress={console.log('\npriceSel: ', priceSel, '\nsortBySel: ', sortBySel, '\nattrSel: ', attrSel)}>Filter Results</Text>
+						{/* back button */}
+						<TouchableOpacity onPress={() => setModalVisible(!modalVisible)} style={styles.filterExitButtons}>
+							<Text style={[styles.buttonText, {color: colors.feastBlue}]}>{"Back"}</Text>
+						</TouchableOpacity>
 
+
+						<Text style={[styles.modalHeader, {fontSize: 30}]} onPress={console.log('\npriceSel: ', priceSel, '\nsortBySel: ', sortBySel, '\nattrSel: ', attrSel)}>Filter Results</Text>
+						<View style={styles.horizontalLine}/>
 						{/* Container for 'Sort By' and 'Price */}
 						<View style = {styles.sortPriceView}>
 
@@ -242,15 +248,16 @@ export default function SearchScreen() {
 										<View 
 											key={sorting} 
 											style={[
-												{borderWidth: checkSortBySel(sorting)? 1:0}, 
+												
 												{width: 95}, 
 												{alignItems: 'center'}, 
 												{borderRadius: 8}, 
-												{backgroundColor: checkSortBySel(sorting)? 'black':"#00000000"}
+												
 											]}
 										> 
-											<TouchableOpacity onPress={() => handleSortBySel(sorting)}>
-												<Text style={[{color:checkSortBySel(sorting)? 'white':'black'}]}>
+											<TouchableOpacity onPress={() => handleSortBySel(sorting)} style={{flexDirection: 'row', alignSelf: 'flex-start'}}>
+												<Ionicons name={checkSortBySel(sorting)? 'ellipse': 'ellipse-outline'} size={20} color={colors.white}/>
+												<Text style={[{color:colors.white, marginLeft: 3}]}>
 													{sorting.replaceAll('_', ' ')}
 												</Text>
 											</TouchableOpacity>
@@ -267,15 +274,16 @@ export default function SearchScreen() {
 										<View 
 											key={p.number} 
 											style={[
-												{borderWidth: checkPriceSelect(p.number)? 1:0}, 
+												
 												{width: 50}, 
 												{alignItems: 'center'}, 
 												{borderRadius: 8}, 
-												{backgroundColor: checkPriceSelect(p.number)? 'black':"#00000000"}
+												
 											]} 
 										>
-											<TouchableOpacity style = {[{width:50}, {alignItems:'center'}]} onPress={() => [handlePriceSel(p.number)]}>
-												<Text style={[{color:checkPriceSelect(p.number)? 'white':'black'}]} >
+											<TouchableOpacity style = {[{width:50}, {alignSelf: 'flex-start'}, {flexDirection: 'row'}]} onPress={() => [handlePriceSel(p.number)]}>
+												<Ionicons name={checkPriceSelect(p.number)? 'ellipse': 'ellipse-outline'} size={20} color={colors.white}/>
+												<Text style={[{color:colors.white}, {marginLeft: 3}]} >
 													{p.price}
 												</Text>
 											</TouchableOpacity>
@@ -284,7 +292,7 @@ export default function SearchScreen() {
 								})}
 							</View>
 						</View>
-							
+						<View style={styles.horizontalLine}/>
 						<Text style={[styles.buttonText, {paddingTop:10}]} >Attributes</Text>
 						{/* View for the 'Attributes' filter by Kenny Du */}
 						<View style={styles.attributesFilter}>
@@ -294,13 +302,11 @@ export default function SearchScreen() {
 										key={attribute} 
 										style={[
 											{padding: 5}, 
-											{borderRadius: 8}, 
-											{borderWidth: checkAttrSel(attribute)? 1:0}, 
-											{backgroundColor: checkAttrSel(attribute)? 'black':"#00000000"}
 										]} 
 									>
-										<TouchableOpacity style={{justifyContent:'center'}} onPress={() => handleAttrSel(attribute)}>
-											<Text style={[{color:checkAttrSel(attribute)? 'white':'black'}]} >
+										<TouchableOpacity style={[styles.attributeContent]} onPress={() => handleAttrSel(attribute)}>
+											<Ionicons name={checkAttrSel(attribute)? 'ellipse': 'ellipse-outline'} size={20} color={colors.white}/>
+											<Text style={[{color:colors.white}]} >
 												{attribute.replaceAll('_', ' ')} 
 											</Text>
 										</TouchableOpacity>
@@ -310,19 +316,12 @@ export default function SearchScreen() {
 						</View>
 
 						{/* View for the 'Back' and 'Save buttons */}
-						<View style = {styles.filterExitButtonsContainer}>
-							<TouchableOpacity
-								onPress={() => setModalVisible(!modalVisible)}
-								style={styles.filterExitButtons}
-							>
-								<Text style={styles.buttonText}>{"Back"}</Text>
-							</TouchableOpacity>
-							
+						<View style = {styles.filterExitButtonsContainer}>							
 							<TouchableOpacity
 								onPress={saveFilters}
-								style={styles.filterExitButtons}
+								
 							>
-								<Text style={styles.buttonText}>{"Save"}</Text>
+								<Text style={{fontSize: 16, color: colors.white, fontWeight: '700'}}>{"Save"}</Text>
 							</TouchableOpacity>
 						</View>
 					</View>
@@ -470,15 +469,16 @@ const styles = StyleSheet.create({
 		marginBottom: 35
 	},
 	buttonText: {
-		color: colors.black,
+		color: colors.white,
 		fontWeight: '700',
 		fontSize: 16
 	},
 	modalView: {
-        margin: 20,
-        height: 510,
-        backgroundColor: '#a2bef0',
-        borderRadius: 20,
+        margin: 0,
+        height: '100%',
+        backgroundColor: colors.backgroundDark,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
         padding: 35,
         alignItems: 'center',
         shadowColor: '#000',
@@ -487,8 +487,13 @@ const styles = StyleSheet.create({
           height: 2,
         },
 		borderWidth:.1,
-		borderBottomColor: 'red'
+		borderBottomColor: 'red',
     },
+	modalHeader: {
+		fontWeight: '700',
+		color: colors.white
+
+	},
 	filterComponent1: {
 		flexDirection: 'column',
 		borderRadius: 5,
@@ -504,7 +509,7 @@ const styles = StyleSheet.create({
 	sortPriceView: {
 		flexDirection: 'row',
 		alignItems: 'center',
-		borderBottomWidth:1,
+		
 		paddingBottom:10,
 		// borderWidth: 1,
 		borderColor: 'black'
@@ -513,30 +518,45 @@ const styles = StyleSheet.create({
 		alignItems: 'center'
 	},
 	attributesFilter:{
-		height: 140, 
-		width: 340,
-		paddingHorizontal: 30,
-		flexDirection: 'column',
-		flexWrap: 'wrap',
-		// borderWidth: 1,
-		borderRadius: 5,
-		borderColor: 'red',
-		alignItems: 'center',
-		alignSelf: 'center',
-		justifyContent: 'space-between',
-	},
-	filterExitButtons:{
-		backgroundColor: '#75d9fc',
+		height: 225, 
 		width: '100%',
-		padding: 15,
-		borderRadius: 50,
-		alignItems: 'center',
-		width: 120,
-		margin: 10,
-		marginTop: 10,
+		//paddingHorizontal: 30,
+		alignContent: 'center'
+		,
+		justifyContent: 'center'
+		,
+		
+		alignSelf: 'baseline',
+		alignItems: 'baseline',
+		
+		flexWrap: 'wrap',
+		flexDirection: 'column',
+		// borderWidth: 1,
 	},
 	filterExitButtonsContainer:{
-		flexDirection: 'row', 
-		justifyContent: 'center',
+		backgroundColor: colors.backgroundDarker,
+		width: '100%',
+		alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: colors.black,
+        borderWidth: 1,
+        height: 60,
+		borderRadius: 15
+	},
+	filterExitButtons: {
+		alignSelf: 'flex-start',
+	},
+	horizontalLine:{
+        height: 1.5, 
+        width:'100%',
+        backgroundColor: colors.white
+    },
+	attributeContent: {
+		height: 40,
+		width: 115,
+		
+		flexDirection: 'row',
+		
+		
 	}
 })
