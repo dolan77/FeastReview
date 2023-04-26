1d86814aba02f805e0aa6c228ab0daeddcaeb19b
nathan-lai-engineering <42527870+nathan-lai-engineering@users.noreply.github.com>
2023-04-25 22:49:27 -0700

    tasteometer fix

--- utils/tasteometer.js
+++ utils/tasteometer.js
@@ -55,7 +55,7 @@ function adjectivesSentimentIncrement(review, restaurantAlias){
     });
     let sentences = review.match( /[^\.!\?]+[\.!\?]+/g ); // split review into sentences
     let toUpdate = {};
-    if(sentences != null && sentences.length <= 1) {
+    if(sentences == null || sentences.length <= 1) {
         sentences = [review]
     }
     if(sentences != null){
@@ -77,6 +77,7 @@ function adjectivesSentimentIncrement(review, restaurantAlias){
                 }
             });
         });
+        console.log(sentences)
         return firebase.dbIncrement("restaurants", restaurantAlias, toUpdate);
     }
     return null;
